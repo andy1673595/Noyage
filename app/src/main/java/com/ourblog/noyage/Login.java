ace0021cb02c9d35d94a4da5e88c9df84dd635b0
Andy_Huang <andy5953761@gmail.com>
2018-04-10 09:59:06 +0800

    two

---
+++ app/src/main/java/com/ourblog/noyage/Login.java
@@ -0,0 +1,17 @@
+package com.ourblog.noyage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void xxx () {
+        
+    }
+}
